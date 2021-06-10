b0646bdb9c73c1abb43c057867b57bf3b1342518
JecosineMoc <mj_ol@outlook.com>
2021-06-10 11:41:03 +0800

    UPDATE

--- src/main/java/com/catalina/infectious/common/ResultCode.java
+++ src/main/java/com/catalina/infectious/common/ResultCode.java
@@ -2,18 +2,16 @@ package com.catalina.infectious.common;
 
 public enum ResultCode {
     //ResultCode的单例对象
-    LOGIN_SUCCESS(200,"Login success"),
-    FAIL(9000,""),
+    LOGIN_SUCCESS(200, "Login success"),
+    FAIL(9000, ""),
     NOT_FOUND(400, "Not found"),
     INTERNAL_ERROR(500, "Internal error"),
     ACCESS_DENIED(403, "Access denied");
 
-
-
     private int code;
     private String msg;
 
-    ResultCode(int code,String msg){
+    ResultCode(int code, String msg) {
         this.code = code;
         this.msg = msg;
     }
